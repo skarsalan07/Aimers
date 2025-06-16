a2359dd1cf296e1688ee48badf85fa358e9d8732
Arsalan Shaikh <113293633+skarsalan07@users.noreply.github.com>
2025-06-16 19:41:52 +0530

    Update home.jsx

--- Development/frontend/src/pages/home.jsx
+++ Development/frontend/src/pages/home.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect } from 'react';
-import Nav from '../components/nav';
-import Curosoul from '../components/main';
-import WhyChooseUs from '../components/why_choose_us';
-import Recommendation from '../components/recommendation';
-import Roadmap from '../components/roadmap';
-import Footer from '../components/footer';
+
 
 const Header = () => {
   useEffect(() => {
